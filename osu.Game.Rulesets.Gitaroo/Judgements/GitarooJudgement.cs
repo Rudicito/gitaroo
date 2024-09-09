f6236829faed01519bd8ce4d33d994f13c901dc4
Rudi Herouard <rudi.herouard@gmail.com>
2024-09-09 13:53:44 +0200

    Add custom Judgement
    Use them in HitObject
    Set max result to great, not perfect anymore

---
+++ osu.Game.Rulesets.Gitaroo/Judgements/GitarooJudgement.cs
@@ -0,0 +1,9 @@
+using osu.Game.Rulesets.Judgements;
+using osu.Game.Rulesets.Scoring;
+
+namespace osu.Game.Rulesets.Gitaroo.Judgements;
+
+public class GitarooJudgement : Judgement
+{
+    public override HitResult MaxResult => HitResult.Great;
+}
--- osu.Game.Rulesets.Gitaroo/Objects/GitarooHitObject.cs
+++ osu.Game.Rulesets.Gitaroo/Objects/GitarooHitObject.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Game.Rulesets.Gitaroo.Judgements;
 using osu.Game.Rulesets.Judgements;
 using osu.Game.Rulesets.Objects;
 
@@ -8,6 +9,6 @@ namespace osu.Game.Rulesets.Gitaroo.Objects
 {
     public class GitarooHitObject : HitObject
     {
-        public override Judgement CreateJudgement() => new Judgement();
+        public override Judgement CreateJudgement() => new GitarooJudgement();
     }
 }
