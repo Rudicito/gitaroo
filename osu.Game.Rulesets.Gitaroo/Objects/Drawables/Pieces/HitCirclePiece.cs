c0c35516c7996b010d24cbec9876065698da371c
Rudi Herouard <rudi.herouard@gmail.com>
2025-03-19 20:07:56 +0100

    Use external color var in HitCirclePiece

--- osu.Game.Rulesets.Gitaroo/Objects/Drawables/Pieces/HitCirclePiece.cs
+++ osu.Game.Rulesets.Gitaroo/Objects/Drawables/Pieces/HitCirclePiece.cs
@@ -1,3 +1,4 @@
+using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
@@ -8,18 +9,21 @@ namespace osu.Game.Rulesets.Gitaroo.Objects.Drawables.Pieces;
 
 public partial class HitCirclePiece : CircularContainer
 {
+    private static readonly Color4 border_colour = new Color4(251, 151, 75, byte.MaxValue);
+    private static readonly Color4 colour = border_colour.Opacity(0.5f);
+
     public HitCirclePiece()
     {
         Anchor = Anchor.Centre;
         Origin = Anchor.Centre;
         Size = new Vector2(20);
-        BorderColour = new Color4(251, 151, 75, byte.MaxValue);
+        BorderColour = border_colour;
         BorderThickness = 2.5f;
         Masking = true;
         AddInternal(new Box
         {
             RelativeSizeAxes = Axes.Both,
-            Colour = new Color4(251, 151, 75, 128),
+            Colour = colour,
         });
     }
 }
