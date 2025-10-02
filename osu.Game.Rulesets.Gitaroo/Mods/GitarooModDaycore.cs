dca997d8ea61bfb34a07a0f08cc07fe59d981b18
Rudi Herouard <rudi.herouard@gmail.com>
2025-10-02 19:29:59 +0200

    Add basic mods

---
+++ osu.Game.Rulesets.Gitaroo/Mods/GitarooModDaycore.cs
@@ -0,0 +1,7 @@
+using osu.Game.Rulesets.Mods;
+
+namespace osu.Game.Rulesets.Gitaroo.Mods;
+
+public class GitarooModDaycore : ModDaycore
+{
+}
---
+++ osu.Game.Rulesets.Gitaroo/Mods/GitarooModMuted.cs
@@ -0,0 +1,8 @@
+using osu.Game.Rulesets.Gitaroo.Objects;
+using osu.Game.Rulesets.Mods;
+
+namespace osu.Game.Rulesets.Gitaroo.Mods;
+
+public class GitarooModMuted : ModMuted<GitarooHitObject>
+{
+}
