1fce714ef8825565ca82ed528210ec484d06fcf4
Rudi Herouard <rudi.herouard@gmail.com>
2025-11-08 15:02:59 +0100

    Add autopilot

---
+++ osu.Game.Rulesets.Gitaroo/Mods/GitarooModAutopilot.cs
@@ -0,0 +1,26 @@
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Localisation;
+using osu.Game.Graphics;
+using osu.Game.Rulesets.Gitaroo.Objects;
+using osu.Game.Rulesets.Gitaroo.UI;
+using osu.Game.Rulesets.Mods;
+using osu.Game.Rulesets.UI;
+
+namespace osu.Game.Rulesets.Gitaroo.Mods;
+
+public class GitarooModAutopilot : Mod, IApplicableToDrawableRuleset<GitarooHitObject>
+{
+    public override string Name => "Autopilot";
+    public override string Acronym => "AP";
+    public override IconUsage? Icon => OsuIcon.ModAutopilot;
+    public override ModType Type => ModType.Automation;
+    public override LocalisableString Description => @"TraceLines automatically tracked - just follow the rhythm.";
+    public override double ScoreMultiplier => 0.1;
+
+    public void ApplyToDrawableRuleset(DrawableRuleset<GitarooHitObject> drawableRuleset)
+    {
+        var gitarooRuleset = (DrawableGitarooRuleset)drawableRuleset;
+
+        gitarooRuleset.Playfield.FanShaped.Auto = true;
+    }
+}
