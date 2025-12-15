35ae05b24361982212ed782acdbcb4d65bef1d83
Rudi Herouard <rudi.herouard@gmail.com>
2025-12-15 12:27:01 +0100

    Fix incompatible mods for autopilot

--- osu.Game.Rulesets.Gitaroo/Mods/GitarooModCinema.cs
+++ osu.Game.Rulesets.Gitaroo/Mods/GitarooModCinema.cs
@@ -1,7 +1,9 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using osu.Game.Beatmaps;
 using osu.Game.Rulesets.Gitaroo.Objects;
 using osu.Game.Rulesets.Gitaroo.Replays;
@@ -11,6 +13,8 @@ namespace osu.Game.Rulesets.Gitaroo.Mods;
 
 public class GitarooModCinema : ModCinema<GitarooHitObject>
 {
+    public override Type[] IncompatibleMods => base.IncompatibleMods.Concat(new[] { typeof(GitarooModAutopilot) }).ToArray();
+
     public override ModReplayData CreateReplayData(IBeatmap beatmap, IReadOnlyList<Mod> mods)
         => new ModReplayData(new GitarooAutoGenerator(beatmap).Generate(), new ModCreatedUser { Username = "Puma" });
 }
